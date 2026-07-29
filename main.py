61ef61e9749141fe4607f4c5683c409862d623e7
Faxriyy <hnlx22@outlook.com>
2026-07-29 12:32:30 +0500

    Establish PostgreSQL connection in main.py
    Added PostgreSQL connection setup with error handling.

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
 from fastapi import FastAPI
 app = FastAPI()
+import psycopg2
 
 @app.get("/")
 def get_home():
     return {"message": "Welcome to home page"}
+
+try:
+    conn = psycopg2.connect(
+        host="sakura.proxy.rlwy.net:52174",
+        port=5432,
+        database="rooms",
+        user="postgres",
+        password="XUcHwwUHmaaFigVzOlpYIdrPZrGwpjKf"
+    )
+
+    print("✅ Connected to PostgreSQL!")
+
+except Exception as e:
+    print("❌ Connection failed!")
+    print(e)
